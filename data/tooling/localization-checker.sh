8068313c85ca906faf338c144a9b4927de7d58d3
Louis-Guillaume MORAND <lgm@lgmorand.com>
2020-06-04 08:12:59 +0200

    fix error from previous commit

--- data/tooling/localization-checker.sh
+++ data/tooling/localization-checker.sh
@@ -9,14 +9,7 @@ echo  "#   Localization checker      #" 2>&1 | tee -a $logfile
 echo  "###############################" 2>&1 | tee -a $logfile
 
 echo "Installing required tools: jq for parsing JSON"
-sudo apt-get install jq > /dev/null
-
-
-CompareLang "en" "fr"
-CompareLang "en" "es"
-
-echo ""
-echo -e "\e[92mComparison done. Open $logfile \e[0m"  
+sudo apt-get install jq -y > /dev/null
 
 function CompareLang()
 (
@@ -46,12 +39,15 @@ function CompareLang()
                 echo -e "$targetFile does not exist"  2>&1 | tee -a $logfile
             fi
         done
+
+        echo -e "Please fix missing files and rerun the tool to search for missing content"  2>&1 | tee -a $logfile
     else
         echo -e "Same number of files between '$source' and '$target'"  2>&1 | tee -a $logfile
+        echo -e "Checking if files content are the same:"  2>&1 | tee -a $logfile
         for f in $sourceFolder/*
         do
             FILENAME=`basename ${f}`;
-            echo -e "comparing $f to $targetFolder/$FILENAME"  2>&1 | tee -a $logfile
+            # echo -e "comparing $f to $targetFolder/$FILENAME"  2>&1 | tee -a $logfile
             sourceCount="$(jq '. | length' $f)"
             targetCount="$(jq '. | length' $targetFolder/$FILENAME)"
             
@@ -63,8 +59,10 @@ function CompareLang()
         done
     fi
 
-
-
 )
 
+CompareLang "en" "fr"
+CompareLang "en" "es"
 
+echo ""
+echo -e "\e[92mComparison done. Open $logfile \e[0m"  
