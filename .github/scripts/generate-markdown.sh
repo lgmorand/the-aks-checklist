adc19c573ce687ca663a329f5c64d72ed6577cf6
Louis-Guillaume MORAND <lgm@lgmorand.com>
2021-04-30 20:49:55 +0200

    Create generate-markdown.sh

---
+++ .github/scripts/generate-markdown.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Loop on each file present in /data/en
+# For each file, create a corresponding markdown file
+# Create a title based on file named (for the moment)
+# Create content based 
