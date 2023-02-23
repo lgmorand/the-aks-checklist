1eb84e51bb52b9c93cb46d76e88d268683af5ec9
Louis-Guillaume MORAND <lgm@lgmorand.com>
2023-02-23 21:39:58 +0100

    adding control to ensure that all items have a GUID

--- .github/scripts/check-unique-guid.sh
+++ .github/scripts/check-unique-guid.sh
@@ -10,13 +10,22 @@ for file in *.json
 do
   echo -e "📜 \e[4mPROCESSING ${file^^}\e[0m"
  
+  all_items_count=$(jq '. | length'  $file)
+
+  echo "INFO: File $file has $all_items_count items"
+
   all_guids_count=$(cat $file | jq -r "try .. | objects | select( .$key_name ) | .$key_name " | wc -l)
+
+  f [ "$all_items_count" -ne "$all_guids_count" ]; then
+    echo "ERROR: File $file has some elements without GUID"
+  else
+
   unique_guids_count=$(cat $file | jq -r "try .. | objects | select( .$key_name ) | .$key_name " | sort -u | wc -l)
 
   if [ "$all_guids_count" -eq "$unique_guids_count" ]; then
     echo -e " \e[0;32mOK\e[0m";
   else
-    echo "INFO: File $file has $all_guids_count GUIDs, but only $unique_guids_count are unique"
+    echo "ERROR: File $file has $all_guids_count GUIDs, but only $unique_guids_count are unique"
     echo "Here are the duplicates:"
     all_guids=$(cat $file | jq -r "try .. | objects | select( .$key_name ) | .$key_name " )
 
