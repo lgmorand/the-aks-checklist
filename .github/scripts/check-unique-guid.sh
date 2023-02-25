43d9ed2b26830574bb06c9f95f92eda4fb3ff3c1
Louis-Guillaume MORAND <lgm@lgmorand.com>
2023-02-25 09:52:18 +0100

    adding script to check that data is OK

--- .github/scripts/check-unique-guid.sh
+++ .github/scripts/check-unique-guid.sh
@@ -2,36 +2,53 @@
 
 echo "Checking that all GUIDs are unique"
 echo ''
-key_name="guid"
 
-echo $key_name
+key_name="guid"
+globalError=0
+
+cd ./data/en/items
 
 for file in *.json
 do
-  echo -e "📜 \e[4mPROCESSING ${file^^}\e[0m"
+  echo -e "?? \e[4mPROCESSING ${file^^}\e[0m"
  
+  hasError=0
+
   all_items_count=$(jq '. | length'  $file)
 
   echo "INFO: File $file has $all_items_count items"
 
   all_guids_count=$(cat $file | jq -r "try .. | objects | select( .$key_name ) | .$key_name " | wc -l)
 
-  f [ "$all_items_count" -ne "$all_guids_count" ]; then
-    echo "ERROR: File $file has some elements without GUID"
-  else
+  if [ "$all_items_count" -ne "$all_guids_count" ]; then
+    echo -e "\e[0;31mERROR\e[0m: File $file has some elements without GUID ($all_guids_count GUID found)"
+    hasError=1
+  fi
 
   unique_guids_count=$(cat $file | jq -r "try .. | objects | select( .$key_name ) | .$key_name " | sort -u | wc -l)
 
-  if [ "$all_guids_count" -eq "$unique_guids_count" ]; then
-    echo -e " \e[0;32mOK\e[0m";
-  else
-    echo "ERROR: File $file has $all_guids_count GUIDs, but only $unique_guids_count are unique"
+  if [ "$all_guids_count" -ne "$unique_guids_count" ]; then
+
+    echo -e "\e[0;31mERROR\e[0m: File $file has $all_guids_count GUIDs, but only $unique_guids_count are unique"
     echo "Here are the duplicates:"
     all_guids=$(cat $file | jq -r "try .. | objects | select( .$key_name ) | .$key_name " )
 
     printf '%s\n' "${all_guids[@]}"|awk '!($0 in seen){seen[$0];next} 1'
+    hasError=1
   fi
 
-
+  if [[ "$hasError" == 1 ]]; then
+    echo -e " \e[0;31mKO\e[0m";
+  else
+    echo -e " \e[0;32mOK\e[0m";
+  fi
+   
   echo ''
 done
+
+if [[ "$globalError" == 1 ]]; then
+    exit 1
+fi
+
+
+cd ../../..
